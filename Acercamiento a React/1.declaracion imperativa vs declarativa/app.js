b51938f8990540c0e905965e0d9ff184e257ca8e
Goldfinch <facu_vila@hotmail.com>
2023-07-31 14:28:08 -0300

    crea un componente

---
+++ Acercamiento a React/1.declaracion imperativa vs declarativa/app.js
@@ -0,0 +1,8 @@
+const div = document.getElementById('div');
+
+function Header(){
+    return <h1>Pizza de peperoni  🍕</h1>
+}
+
+ReactDOM.render(<Header />, div);
+
---
+++ Acercamiento a React/2.crear un componente/app.js
@@ -0,0 +1,8 @@
+const div = document.getElementById("div");
+
+//los componentes de React deben estar en mayúsculas para distinguirlos de HTML y JavaScript simples.
+function Header() {
+  return <h1>Pizza de peperoni 🍕</h1>;
+}
+//usa los componentes de React de la misma manera que usaría las etiquetas HTML normales, con corchetes angulares
+ReactDOM.render(<Header />, div);
